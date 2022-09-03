fa7b643e0e3dfba28475c87bd823d9486f524614
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-09-03 16:24:42 +0800

    Set Dynamic Head for Filtered Event

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -3,6 +3,7 @@
 import { Fragment, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import useSWR from 'swr';
+import Head from 'next/head';
 
 import { getFilteredEvents } from '../../helpers/api-util';
 import EventList from '../../components/events/event-list';
@@ -97,6 +98,10 @@ function FilteredEventsPage(props) {
 
   return (
     <Fragment>
+      <Head>
+        <title>Filtered Events</title>
+        <meta name='description' content={`All Event(s) for ${numMonth}/${numYear}`} />
+      </Head>
       <ResultsTitle date={date} />
       <EventList items={filteredEvents} />
     </Fragment>
