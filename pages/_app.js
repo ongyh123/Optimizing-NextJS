5290616cfa0e1bec5665a4720e5c40c53bbb2c25
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-09-03 17:38:19 +0800

    Merging Head Component

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+//title here will be override by page specific data
+
 import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
@@ -7,6 +9,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
